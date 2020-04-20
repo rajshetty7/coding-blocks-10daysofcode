c7f42247871f35755ad3e85d234ae0a1201d23e6
Raj Shetty <41022926+rajshetty7@users.noreply.github.com>
2020-04-20 16:46:46 +0530

    Trailing Zeroes

---
+++ trailing_zeroes_factorial.cpp
@@ -0,0 +1,50 @@
+/*
+Trailing Zeroes
+
+Given an integer n, write a function that returns count of trailing zeroes in n!.
+
+Input Format
+A single integer N.
+
+Constraints
+1 <= N <= 10^9
+
+Output Format
+A single integer denoting the count of trailing zeroes in N!
+
+Sample Input
+5
+
+Sample Output
+1
+
+Explanation
+Factorial of 5 is 120 which has one trailing 0.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int trailingZeroes(int n) {
+    int ans = 0;
+    int p = 5;
+    
+    while((n/p) > 0) {
+        ans += (n/p);
+        p *= 5;
+    }
+
+    return ans;
+}
+
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    
+    cout<<trailingZeroes(n)<<endl;
+	
+    return 0;
+}
